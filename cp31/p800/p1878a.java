57d50bbf62fb47c4e84de8c14a130ccdde9fa604
Pratik Survase <survasepratik1304@gmail.com>
2026-01-27 10:56:59 +0530

    done

---
+++ cp31/p800/p1878a.java
@@ -0,0 +1,15 @@
+package cp31.p800;
+public class p1878a {
+    public boolean isPresent(int[] nums,int k){
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==k){
+                return true;
+            }
+        }
+        return false;
+    }
+  public static void main(String[] args) {
+    
+  }
+    
+}
