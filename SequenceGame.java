4099fbcb0a981754e0cfab75a8538ccb4d7996d9
Pratik Survase <survasepratik1304@gmail.com>
2026-05-21 18:35:57 +0530

    done

---
+++ SequenceGame.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SequenceGame {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] b = new int[n];
+            for (int i = 0; i < n; i++) {
+                b[i] = sc.nextInt();
+            }
+            ArrayList<Integer> a = new ArrayList<>();
+            a.add(b[0]);
+            for (int i = 1; i < n; i++) {
+                if (b[i] >= b[i - 1]) {
+                    a.add(b[i]);
+                } else {
+                    a.add(1);
+                    a.add(b[i]);
+                }
+            }
+            System.out.println(a.size());
+            for(int x:a){
+                System.out.println(x+" ");
+            }
+            System.out.println();
+
+        }
+    }
+}
